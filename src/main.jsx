f3c4f205c8c094c83b4003953a6ceaa410c1c4a2
anil-gehlot <anilgehlot2000@gmail.com>
2024-11-10 09:06:20 +0530

    entry point of the react app

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import '@vetixy/circular-std' // To include the circular-std fonts.
+
+
+createRoot(document.getElementById("root")).render(<App />);
