d9b80aadcb3d40847bb806d9f04361ddf3abf550
anil-gehlot <anilgehlot2000@gmail.com>
2024-11-10 09:22:55 +0530

    Top level component

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Tabs from "./components/Tabs";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div>
+        <Tabs />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
