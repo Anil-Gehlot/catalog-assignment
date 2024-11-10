93cb7ec11c61e7a6fb560b7ded9225356e375497
anil-gehlot <anilgehlot2000@gmail.com>
2024-11-10 09:20:09 +0530

    content of setting tab

---
+++ src/components/Settings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Settings = () => {
+  return (
+    <div className="summary-container">
+    <h2>This is setting section</h2>
+    </div>
+  )
+}
+
+export default Settings
