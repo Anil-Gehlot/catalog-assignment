9674377db85938e1946ebb6ae0f3c33a6d09125a
anil-gehlot <anilgehlot2000@gmail.com>
2024-11-10 09:19:48 +0530

    content of analysis tab

---
+++ src/components/Analysis.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Analysis = () => {
+  return (
+    <div className="summary-container">
+      <h2>Analysis sections</h2>
+      <h3>Short term Analysis</h3>
+      <p>
+        Bitcoin has broken the rising trend up in the short term, which
+        indicates an even stronger rising rate. There is no resistance in the
+        price chart and further rise is indicated. In case of a negative
+        reaction, the currency has support at approximately 66000 points.
+        Positive volume balance strengthens the currency in the short term. RSI
+        is above 70 after a good price increase the past weeks. The currency has
+        strong positive momentum and further increase is indicated. However,
+        particularly for big stocks, high RSI may be a sign that the stock is
+        overbought and that there is a chance of a reaction downwards. The
+        currency is overall assessed as technically positive for the short term.{" "}
+      </p>
+
+      <h3>Medium term Analysis</h3>
+      <p>
+        Medium term Bitcoin has broken up from an approximate horizontal trend
+        channel in the medium long term after investors have paid ever more. A
+        positive signal has been triggered and further increase for the currency
+        is indicated. It also gave positive signal from the rectangle formation
+        at the break up through the resistance at 67241. Further rise to 84547
+        or more is signaled. There is no resistance in the price chart and
+        further rise is indicated. In case of a negative reaction, the currency
+        has support at approximately 71000 points. The currency is overall
+        assessed as technically positive for the medium long term.
+      </p>
+
+      <h3>Long term Analysis</h3>
+      <p>
+        Bitcoin is in a rising trend channel in the long term. This shows that
+        investors over time have bought the currency at higher prices and
+        indicates good development for the currency. The currency has given a
+        positive signal from the rectangle formation by a break up through the
+        resistance at 67241. Further rise to 84547 or more is signaled. The
+        currency has broken up through resistance at points 71000. This predicts
+        a further rise. In case of negative reactions, there will now be support
+        at points 71000. Positive volume balance indicates that buyers are
+        aggressive while sellers are passive, and strengthens the currency. The
+        RSI curve shows a rising trend, which supports the positive trend. The
+        currency is overall assessed as technically positive for the long term.
+      </p>
+    </div>
+  );
+};
+
+export default Analysis;
